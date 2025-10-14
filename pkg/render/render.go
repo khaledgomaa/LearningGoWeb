508a03021c0edae37f083baeccfcc3d80fc7e5fd
Khaled Sayed <khaled.sayed@talabat.com>
2025-10-14 09:31:06 +0300

    Added layout template

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RenderTemplate(w http.ResponseWriter, tml string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tml)
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tml, "./templates/base.layout.tmpl") // Added template file while rendering the templates
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template: ", err)
